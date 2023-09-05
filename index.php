ff90083750e3dbf2546555ea68701d16d19b6201
Mimer <mimer@edngt.com>
2023-09-05 15:07:54 -0600

    Actualizar index.php

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ exit;
 
 <head>
 <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+	
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
